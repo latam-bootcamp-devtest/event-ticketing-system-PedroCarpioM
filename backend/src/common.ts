115f1ba9a8c80c63da489dde5f5e09e7e650a221
PEDRO CARPIO MONTERO <pedrocarpiom@gmail.com>
2025-03-08 13:33:47 -0400

    removed common auth

--- backend/src/common.ts
+++ backend/src/common.ts
@@ -1,8 +1,4 @@
 export const commonHTTPResponses = {
-  401: {
-    description: "Unauthorized request, please provide a valid API key",
-    type: "null",
-  },
   400: {
     description: "Bad request, please check your request body",
     type: "null",
