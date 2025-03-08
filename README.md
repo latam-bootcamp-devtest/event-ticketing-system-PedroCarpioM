f66c58b4f9e3353ee6edde50fa01e7b967299c3a
PEDRO CARPIO MONTERO <pedrocarpiom@gmail.com>
2025-03-08 09:30:23 -0400

    initialize new fastify app with swagger and swagger-ui

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/Eu2FmBb3)
+
 # Event-Ticketing-System
+
+## Planning
+This app has a [Figma board](https://www.figma.com/board/ubbLnIGoLjCbkYh95faysr/DevTest?t=I0GnYIyKCDz8SjCr-1) with all the info
+## Installation
+To install this app checkout inside backend and run:
+```bash
+pnpm i
+```
+
+## Dev run
+To run dev enviroment, configure the required dev variables and execute:
+```bash
+pnpm dev
+```
+## Info
+This project is based on a full lot of ideas!
+
+## Ideas
+[practica.dev](practica.dev)
+[the typescript way](https://www.typescriptlang.org/)
+[the fastify way](https://www.typescriptlang.org/)
+[]
---
+++ backend/src/plugins/openapi.ts
@@ -0,0 +1,43 @@
+import fastifySwagger, { FastifySwaggerOptions } from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import fp from 'fastify-plugin'
+
+/**
+ * This plugins adds a swagger plugin
+ *
+ * @see https://swagger.io
+ */
+export default fp<FastifySwaggerOptions>(async (fastify) => {
+    fastify.register(fastifySwagger, 
+        {
+            openapi: {
+                openapi: '3.0.0',
+                info: {
+                  title: 'Pedro Dev Test Api',
+                  description: 'Dev Test Api to manage event tickets',
+                  version: '0.1.0'
+                },
+                servers: [
+                  {
+                    url: 'http://localhost:3000',
+                    description: 'Development server runing in local'
+                    },
+                    {
+                        url: 'http://localhost:3000',
+                        description: 'Development server runing in local'
+                      }
+                ],
+                tags: [
+                  { name: 'User', description: 'User related end-points' },
+                  { name: 'Booking', description: 'Booking related end-points' },
+                  { name: 'Event', description: 'Event related end-points' }
+                ],
+                externalDocs: {
+                  url: 'https://swagger.io',
+                  description: 'Find more info here'
+                }
+              }
+        }
+    )
+    fastify.register(fastifySwaggerUi)
+})
